cadd4e2641b27367b0cc8f39642b3e7afe05f2db
Silva257 <silvacharlesniyokindi@gmail.com>
2025-03-15 18:08:53 +0300

    4-5 ad js commit

--- advancejs.js
+++ advancejs.js
@@ -1,3 +1,6 @@
+import  Great  from "./library.js";
+import {Greater, Gre}  from "./lib.js";
+
 console.log("hello Silva")
 
 // USING CLASSES IN JAVASCRIPT :
@@ -71,3 +74,92 @@ const myName = new user('Silva')
 console.log(myName.name);
 const myName1 = new user('charles')
 console.log(myName1.name)
+
+// CLASS AND FUNCTIONS #NCE
+
+class love{
+    constructor(name, email, country){
+        this.name = name
+        this.email = email
+        this.country = country
+    }
+    tap(){
+        return `hello ${this.name},${this.email}, ${this.country}`
+    }
+    
+}
+const myLove = new love('sweetHeart', 'ane@gmail.com', 'Burundi')
+console.log(myLove)
+
+const like = (name, email, country)=>{
+    return `myLike is ${name}, myMail is ${email}, country is ${country}`
+}
+const myLike = like('Silva Charles', 'Silva@gmail.com', 'Taiwan')
+console.log(myLike)
+
+// static methods
+
+class myMath{
+    static math(a, b){
+        return a+b
+    }
+}
+console.log(myMath.math(45, 72))
+
+class Class {
+    constructor(student, age){
+        this._student = student
+        this._age = age
+    }
+    // get student(){
+    //     return this._student
+    // }
+    // set student(Stud){
+    //    this._student = Stud
+    // }
+}
+const classe = new Class('Silva', '30');
+console.log(classe); // logs the object
+// classe.student = 'Niyokindi';
+// classe.student = 'charles'
+// classe.student = 'claude'
+// console.log(classe._student)
+
+
+
+// INHERITANCE IN JAVASCRIPT
+
+class Admin extends User{
+    constructor(name, email, role){
+        super(name, email)
+        this.role = role
+    }
+
+    getRole(){
+        return `Your role: ${this.role}`
+    }
+}
+
+    const admin = new Admin('Charles', 'cha@gmail.com', 'superAdmin')
+    console.log(admin.getRole())
+    console.log(admin.greet())
+
+    // MODULES
+
+
+    console.log(Great('Charly'))
+    console.log(Greater('Niyokindi'))
+    console.log(Gre('Dev'))
+
+    // Try and Catch
+
+try{
+    let num = JSON.parse('Invalid JSON')
+        console.log('my code', num)
+    
+}catch(error){
+    console.log('An error occurred :error')
+}finally{
+    console.log('Error handled successifully')
+}
+        
